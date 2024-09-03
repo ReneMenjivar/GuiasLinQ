2e688d792ab107a53e6c92c9f820dd7bd1b347a3
Rene Lemus <lm0298032023@unab.edu.sv>
2024-09-02 21:00:41 -0600

    Añadir archivos de código fuente

---
+++ Casa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroduccionLinQ
+{
+    public class Casa
+    {
+        public int Id { get; set; }
+        public string Direccion { get; set; }
+        public string Ciudad { get; set; }
+        public int numeroHabitaciones { get; set; }
+        public string dameDatosCasa()
+        {
+            return $"Direción es {Direccion} en la ciudad de {Ciudad} con numero de habitaciones {numeroHabitaciones}";
+        }
+    }
+}
